0e407341fc81278d90c9ccad25326483cf3563eb
Louis Pixodeo <louis@pixodeo.net>
2023-05-12 16:11:10 +0700

    init

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# name: discourse-custom-category
+# about: Custom the continent navigation
+# version: 1.0
+# authors: Louis Pixodeo
+# url: https://pixodeo.com/
+
+hide_plugin if self.respond_to?(:hide_plugin)
+
+register_asset "stylesheets/discourse-custom-category.scss"
