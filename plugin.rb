079ac0abcba45a71b3decdfa3708e3240c7ea2c5
Louis Pixodeo <louis@pixodeo.net>
2023-05-17 15:15:32 +0700

    update header menu

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-# name: discourse-custom-category
+# name: discourse-custom-nagvigation
 # about: Custom the continent navigation
 # version: 1.0
 # authors: Louis Pixodeo
 # url: https://pixodeo.com/
 
-register_asset "stylesheets/discourse-custom-category.scss"
+#hide_plugin if self.respond_to?(:hide_plugin)
+
+register_asset "stylesheets/custom-category.scss"
+register_asset "stylesheets/custom-category-mobile.scss", :mobile
